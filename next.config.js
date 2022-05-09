96580496892e6c2904b6d21c12b611d7ee712464
woywro <woywro@gmail.com>
2022-05-09 14:41:53 +0200

    refactor: update nect config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer(
-  (module.exports = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    reactStrictMode: true,
-    images: {
-      domains: ['play.pokemonshowdown.com'],
-      formats: ['image/webp'],
-    },
-  })
-);
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ['play.pokemonshowdown.com'],
+    formats: ['image/webp'],
+  },
+};
